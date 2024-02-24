303fe0386b0e37edb04423e3de4a4c60fe0465f4
Ashkan Yaghobi <ashkan.yaghobii@gmail.com>
2024-02-24 17:18:26 +0330

    CheckOtp configs

--- src/Components/Templates/CheckOtpForm.jsx
+++ src/Components/Templates/CheckOtpForm.jsx
@@ -1,14 +1,38 @@
 import { useEffect } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
-function CheckOtpForm() {
+function CheckOtpForm({ code, setCode, mobile, setStep }) {
   useEffect(() => {
     toast.success("کد با موفقیت ارسال شد ");
-  }, []);
+  }, [mobile]);
+
+  const changemobileHandler = () => {
+    setStep(1);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log({code , mobile});
+  };
+
 
   return (
     <>
-      <div>CheckOtpForm</div>
+      <form onSubmit={submitHandler}>
+        <p>تایید کد ارسال شده برای شما </p>
+        <span>لطفا کد پیامک شده به شماره {mobile} را وارد نمایید .</span>
+        <label htmlFor="input">کد تایید را وارد نمایید</label>
+        <input
+          type="text"
+          id="input"
+          placeholder="کد تایید"
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+        />{" "}
+        <br />
+        <button type="submit">ورود به حساب</button>
+        <button onClick={changemobileHandler}>تغییر شماره موبایل</button>
+      </form>
       <Toaster />
     </>
   );
