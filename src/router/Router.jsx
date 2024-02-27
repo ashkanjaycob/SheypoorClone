ad96a907769a30c2c19c48396ecd4113086c57b5
Ashkan Yaghobi <ashkan.yaghobii@gmail.com>
2024-02-27 03:55:07 +0330

    Loader on Routes added

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import Authpage from "../pages/Authpage";
 import Homepage from "../pages/Homepage";
@@ -6,21 +6,45 @@ import Dashboard from "../pages/Dashboard";
 import AdminPage from "../pages/AdminPage";
 import NotFound from "../pages/NotFound";
 import { getProfile } from "../Services/user";
-
+import { ThreeCircles } from "react-loader-spinner";
+import styles from "../router/loader.module.css";
 
 function Router() {
+  const { data, isLoading, error } = useQuery(["profile"], getProfile);
+  console.log({ data, isLoading, error });
 
-  const { data, isLoading , error } = useQuery(["profile"] , getProfile);
-  console.log({ data, isLoading , error });
-
-  if ( isLoading) return <h1>is loading ... </h1>
+  if (isLoading)
+    return (
+      <div className={styles.loader}>
+        <ThreeCircles
+          visible={true}
+          height="60"
+          width="60"
+          color="#1a90ff"
+          ariaLabel="three-circles-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+      </div>
+    );
 
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/auth" element={<Authpage />} />
-      <Route path="/admin" element={<AdminPage />} />
+      <Route
+        path="/dashboard"
+        element={data ? <Dashboard /> : <Navigate to="/auth" />}
+      />
+      <Route
+        path="/auth"
+        element={data ? <Navigate to="/dashboard" /> : <Authpage />}
+      />
+      <Route
+        path="/admin"
+        element={
+          data && data.role === "ADMIN" ? <AdminPage /> : <Navigate to="/" />
+        }
+      />
       <Route path="/*" element={<NotFound />} />
     </Routes>
   );
